a6a904dee39012ba0878211125059ceb0a4a3dee
jezraelhope <jezraelhope@gmail.com>
2023-05-04 18:26:28 -0500

    Add font-family

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import Arena from './components/Arena';
-import Controls from './components/Controls';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Scoreboard from './components/Scoreboard';
+import Arena from "./components/Arena";
+import Controls from "./components/Controls";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Scoreboard from "./components/Scoreboard";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className='h-screen bg-gray-200 text-gray-800'>
+    <div className="App h-screen bg-gray-200 text-gray-800 font-default">
       <Header />
-      <div className='h-3/4 px-3 flex mx-auto space-x-3 py-7'>
+      <div className="h-3/4 px-3 flex mx-auto space-x-3 py-7">
         <Scoreboard />
         <Arena />
         <Controls />
