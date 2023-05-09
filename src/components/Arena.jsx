61e7f515f79b6cd752cfcb8c462001cf212200e7
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-05-09 09:19:50 -0500

    Add Canvas to Arena Component

--- src/components/Arena.jsx
+++ src/components/Arena.jsx
@@ -1,9 +1,17 @@
-import ArenaGrid from './ArenaGrid';
+import Canvas from './Canvas';
+
+const draw = (context) => {
+  context.fillStyle = 'rgb(200, 0, 0)';
+  context.fillRect(10, 10, 50, 50);
+
+  context.fillStyle = 'rgba(0, 0, 200, 0.5)';
+  context.fillRect(30, 30, 50, 50);
+};
 
 const Arena = () => {
   return (
     <div className='w-full h-full bg-[#1e1e1e] text-[#FFFFFF]'>
-      <ArenaGrid />
+      <Canvas draw={draw} height={400} width={400} />
       <div className='pt-12 text-center'>
         <button
           type='button'
