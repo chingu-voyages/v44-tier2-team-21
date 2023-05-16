75ea039739d8776f24c1b3a8270d3cfd487cea95
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-05-16 13:18:35 -0500

    Update boolfunctions

--- src/utils/BoolFunctions.jsx
+++ src/utils/BoolFunctions.jsx
@@ -1,47 +1,47 @@
-export function ANDOperation(bool1, bool2) {
-  if ((bool1 !== bool2) & (operation === "And")) {
-    return "TIE";
+export function ANDOperation(operation, bool1, bool2) {
+  if ((bool1 !== bool2) & (operation === 'And')) {
+    return 'TIE';
   } else {
-    return "WIN";
+    return 'WIN';
   }
 }
 
 export function OROperation(bool1, bool2) {
   if (bool1 !== bool2) {
-    return "TIE";
+    return 'TIE';
   } else {
-    return "WIN";
+    return 'WIN';
   }
 }
 
-export function XOROperation(bool1, bool2) {
-  if ((bool1 !== bool2) & (operation === "And")) {
-    return "TIE";
+export function XOROperation(operation, bool1, bool2) {
+  if ((bool1 !== bool2) & (operation === 'And')) {
+    return 'TIE';
   } else {
-    return "WIN";
+    return 'WIN';
   }
 }
 
 export function NANDOperation(operation, bool1, bool2) {
-  if ((bool1 !== bool2) & (operation === "And")) {
-    return "TIE";
+  if ((bool1 !== bool2) & (operation === 'And')) {
+    return 'TIE';
   } else {
-    return "WIN";
+    return 'WIN';
   }
 }
 
 export function NOROperation(operation, bool1, bool2) {
-  if ((bool1 !== bool2) & (operation === "And")) {
-    return "TIE";
+  if ((bool1 !== bool2) & (operation === 'And')) {
+    return 'TIE';
   } else {
-    return "WIN";
+    return 'WIN';
   }
 }
 
 export function XNOROperation(operation, bool1, bool2) {
-  if ((bool1 !== bool2) & (operation === "And")) {
-    return "TIE";
+  if ((bool1 !== bool2) & (operation === 'And')) {
+    return 'TIE';
   } else {
-    return "WIN";
+    return 'WIN';
   }
 }
