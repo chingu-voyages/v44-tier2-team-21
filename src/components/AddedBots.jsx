1157881bd9d41ab8da5f7ac863d761ee5c4713b9
jezraelhope <jezraelhope@gmail.com>
2023-05-18 18:35:15 -0500

    #64: refactor bot select code

---
+++ src/components/AddedBots.jsx
@@ -0,0 +1,35 @@
+import { useContext, useReducer } from "react";
+import icon1 from "../assets/ghoss.svg";
+
+import menu from "../assets/menu.svg";
+import { BotContext } from "../context/botcontext/BotState";
+import BotReducer from "../context/botcontext/BotReducer";
+
+const AddedBots = ({ bot }) => {
+  const { botdata, setMainState } = useContext(BotContext);
+
+  return (
+    <div
+      className={`${
+        bot.selected ? "border-red-500" : "border-white"
+      } bot-added border rounded-xl flex flex-row space-x-10 mb-3 p-2 items-center cursor-pointer`}
+      key={bot.id}
+      onClick={() => {
+        const newBotData = botdata.map((elem) => {
+          if (elem.id === bot.id) {
+            elem.selected = !bot.selected;
+          }
+          return elem;
+        });
+        console.log(newBotData);
+        setMainState({ botdata: newBotData });
+      }}
+    >
+      <img src={icon1} alt="" className="w-10" />
+      <p>{bot.name}</p>
+      <img src={menu} alt="" className="w-8" />
+    </div>
+  );
+};
+
+export default AddedBots;
