d3de2089d08ab6a6c8ca58c1bf7d783092ade314
jezraelhope <jezraelhope@gmail.com>
2023-06-06 12:02:19 -0500

    Remove dummy data

--- src/context/botcontext/BotState.jsx
+++ src/context/botcontext/BotState.jsx
@@ -3,48 +3,7 @@ import BotReducer from "./BotReducer";
 
 // DUMMY DATA
 const initialstate = {
-  botdata: [
-    {
-      bool: 1,
-      id: 1,
-      initDirection: "NORTH",
-      name: "gary",
-      operation: "AND",
-      color: "hotpink",
-      selected: false,
-      score: 0,
-    },
-    {
-      bool: 0,
-      id: 2,
-      initDirection: "EAST",
-      name: "cecil",
-      operation: "XOR",
-      color: "red",
-      selected: false,
-      score: 0,
-    },
-    {
-      bool: 1,
-      id: 3,
-      initDirection: "NORTH",
-      name: "blue",
-      operation: "AND",
-      color: "red",
-      selected: false,
-      score: 0,
-    },
-    {
-      bool: 0,
-      id: 4,
-      initDirection: "NORTH",
-      name: "yellow",
-      operation: "AND",
-      color: "blue",
-      selected: false,
-      score: 0,
-    },
-  ],
+  botdata: [],
 };
 
 // CONTEXT
