a2cdb943eb2083f715dc1a1946265d2175fd9388
jezraelhope <jezraelhope@gmail.com>
2023-06-08 15:58:16 -0500

    111: Add optional chaining for when botdata is empty

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,6 +1,6 @@
-import { useContext, useEffect, useState } from 'react';
-import { BotContext } from '../context/botcontext/BotState';
-import LeaderBots from './LeaderBots';
+import { useContext, useEffect, useState } from "react";
+import { BotContext } from "../context/botcontext/BotState";
+import LeaderBots from "./LeaderBots";
 
 const Scoreboard = ({ scores }) => {
   const data = useContext(BotContext);
@@ -17,13 +17,13 @@ const Scoreboard = ({ scores }) => {
       return 0;
     };
 
-    const sortedData = [...data.botdata].sort(compare);
+    const sortedData = [...data?.botdata].sort(compare);
     setLeaderData(sortedData);
   }, [JSON.stringify(data.botdata)]);
 
   return (
-    <div className='ml-6 w-full h-full bg-[#1E1E1E] text-[#FFFFFF] text-center border-4 rounded-md border-[#FF0000] max-w-sm'>
-      <h2 className='mb-7 text-2xl mt-5'>LEADERBOARD</h2>
+    <div className="ml-6 w-full h-full bg-[#1E1E1E] text-[#FFFFFF] text-center border-4 rounded-md border-[#FF0000] max-w-sm">
+      <h2 className="mb-7 text-2xl mt-5">LEADERBOARD</h2>
       <LeaderBots data={leaderData} />
     </div>
   );
