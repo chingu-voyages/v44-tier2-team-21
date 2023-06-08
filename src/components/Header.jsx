9c8ed10dbda8fe23013224950b49b38a71fbd87a
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-06-08 09:46:39 -0500

    Update title in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div className='text-center text-3xl font-bold bg-[#1E1E1E] text-[#D9D9D9] py-3 sticky top-0 w-full'>
-      <h1>BOOLEBOTS-21</h1>
+      <h1>BOOLE BOTS-21</h1>
     </div>
   );
 };
